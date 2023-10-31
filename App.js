56d44bf8c8c6a8473727309b146de86ffdfdeee3
Refusado <refusado@gmail.com>
2023-10-31 05:42:05 -0300

    adding styles

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { StyleSheet, Text, View, Image } from "react-native"
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Pass Generator</Text>
       <Image
         source={require('./assets/password-icon.png')}
         style={styles.logo}
       />
-      <Text style={styles.title}>Pass Generator</Text>
     </View>
   )
 }
@@ -15,13 +15,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#123',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   title: {
-    color: '#def',
+    backgroundColor: '#def',
     fontSize: 16,
+    marginBottom: 16,
+    paddingHorizontal: 16,
+    paddingVertical: 4,
+    borderRadius: 4
   },
   logo: {
-    width: 300,
-    height: 300,
+    resizeMode: 'contain',
+    width: 120,
+    height: 120,
   }
 });
