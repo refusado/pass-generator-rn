5eb69904bd0eb4d4d027b2de86ca8edd11d20be2
Refusado <refusado@gmail.com>
2023-10-31 04:21:31 -0300

    adding elements

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./assets/password-icon.png')}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>Pass Generator</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#123',
   },
+  title: {
+    color: '#def',
+    fontSize: 16,
+  },
+  logo: {
+    width: 300,
+    height: 300,
+  }
 });
