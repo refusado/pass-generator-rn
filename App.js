f2ffd69ee89961509cc34f72bcf47b94852f6bfa
Refusado <refusado@gmail.com>
2023-11-01 14:28:41 -0300

    add main content + slider

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View, Image } from "react-native"
+import Slider from "@react-native-community/slider"
 
 export default function App() {
   return (
@@ -8,6 +9,18 @@ export default function App() {
         source={require('./assets/password-icon.png')}
         style={styles.logo}
       />
+
+      <View style={styles.main}>
+        <Text style={styles.mainText}>14 Characters</Text>
+        <Slider
+          style={{ height: 56 }}
+          minimumValue={8}
+          maximumValue={40}
+          minimumTrackTintColor="#58b1e8"
+          maximumTrackTintColor="#1295e6"
+          thumbTintColor="#d12446"
+        />
+      </View>
     </View>
   )
 }
@@ -31,5 +44,18 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
     width: 120,
     height: 120,
+    marginBottom: 40
+  },
+
+  main: {
+    backgroundColor: '#ddeeff44',
+    paddingVertical: 16,
+    width: '88%',
+    borderRadius: 8
+  },
+  mainText: {
+    color: '#def',
+    textAlign: 'center',
+    fontSize: 24
   }
 });
