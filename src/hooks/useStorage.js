db004d37643fc0694e4a00279fe069278dc37c53
Refusado <refusado@gmail.com>
2023-11-06 02:02:59 -0300

    creating useStorage functions

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,33 +1,45 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
 export default function useStorage() {
-  // Get a storage item
-  const getItems = async () => {
+  // Get saves from storage
+  const getItems = async (key) => {
     try {
-      // to do
+      const saves = await AsyncStorage.getItem(key);
+      return JSON.parse(saves) || [];
+
     } catch (error) {
-      console.log('Error while getting storage item', error);
+      console.log('Error while getting the storage item', error);
       return null;
     }
   }
 
   // Save new item in storage
-  const saveItem = async () => {
+  const saveItem = async (key, value) => {
     try {
-      // to do
+      const saves = await getItems(key);
+      saves.push(value);
+
+      await AsyncStorage.setItem(key, JSON.stringify(saves));
+      return saves;
+
     } catch (error) {
-      console.log('Error while saving storage item', error);
+      console.log('Error while saving value', error);
       return null;
     }
   }
 
-  // Remove a storage item
-  const removeItem = async () => {
+  // Remove item from storage
+  const removeItem = async (key, value) => {
     try {
-      // to do
+      const saves = await getItems(key);
+      saves.filter(item => item != value);
+
+      await AsyncStorage.setItem(key, JSON.stringify(saves));
+      return saves;
+
     } catch (error) {
-      console.log('Error while removing storage item', error);
-      return false;
+      console.log('Error while removing value', error);
+      return null;
     }
   }
 
