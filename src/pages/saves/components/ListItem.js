a47bc3e3d683efbef08c6328488cb912ccf85d1e
Refusado <refusado@gmail.com>
2023-11-09 09:29:08 -0300

    adding a "hidden and show" system

--- src/pages/saves/components/ListItem.js
+++ src/pages/saves/components/ListItem.js
@@ -1,30 +1,61 @@
-import { Pressable, StyleSheet, Text } from "react-native";
+import { AntDesign, Entypo, Feather } from "@expo/vector-icons";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function ListItem({ value, handleDelete }) {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <Pressable
       onLongPress={handleDelete}
       style={styles.container}
       android_ripple={{ color: '#ff313144' }}
     >
-      <Text style={styles.text}>{value}</Text>
+      {isVisible ? 
+        <Text style={styles.text}>{value}</Text> :
+        <View style={[styles.censoredText, { width: value.length * 9 }]} />}
+
+      <Pressable
+        style={styles.eyeButton}
+        onPress={() => setIsVisible((prev) => !prev)}
+      >
+        {isVisible ? 
+          <Feather name="eye" size={24} color="#3b82c9" /> :
+          <Feather name="eye-off" size={24} color="#3b82c9" />}
+      </Pressable>
     </Pressable>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
     gap: 8,
     marginHorizontal: 16,
     marginBottom: 16,
-    paddingHorizontal: 12,
-    paddingVertical: 8,
     backgroundColor: '#ddeeff44',
     borderRadius: 2
   },
   text: {
-    color: '#c9dbf6'
+    color: '#c9dbf6',
+    marginHorizontal: 12,
+    marginVertical: 8,
+  },
+  censoredText: {
+    backgroundColor: '#ddeeff44',
+    height: 21,
+    borderRadius: 999,
+    marginHorizontal: 12,
+    marginVertical: 8,
+  },
+  eyeButton: {
+    paddingLeft: 24,
+    paddingRight: 12,
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
